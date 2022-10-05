14a283ebcd0a0c387a73b35e076e8af9ae041bf6
OctaFusari <fusarioctavian@gmail.com>
2022-10-05 21:19:38 +0200

    MODS SQUADRA

--- src/app/components/profilo/squadra/squadra-container/squadra-container.component.ts
+++ src/app/components/profilo/squadra/squadra-container/squadra-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ref, onValue, getDatabase } from 'firebase/database';
 
 @Component({
   selector: 'app-squadra-container',
@@ -7,10 +8,28 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SquadraContainerComponent implements OnInit {
   @Input() userData:any = "";
+  sezioneSquadra:number = 0;
+  arraySquadre: any[] = [];
+  squadraScelta:any = "";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cercaSquadra(squadravalue:any){
+    this.arraySquadre = [];
+    this.sezioneSquadra = 1
+    const db = getDatabase();
+    const starCountRef2 = ref(db, 'squadre/');
+    onValue(starCountRef2, (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+          if (((childSnapshot.val().nomesquadra.includes(squadravalue)) || (childSnapshot.val().codicesquadra.includes(squadravalue)))) {
+            this.arraySquadre.push(childSnapshot.val());
+          }
+      })
+    })
+
   }
 
 }
